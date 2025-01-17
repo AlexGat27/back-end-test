5436e4abc7fcfeb92680fb225af1e57606eddd1d
Александр Гатаулин <a.gataulin@vald.ai>
2025-01-17 12:07:52 +0300

    fix: Changes in adapter index

--- src/adapter/index.ts
+++ src/adapter/index.ts
@@ -1,17 +1,21 @@
 import { buildExampleGateway, ExampleGateway } from './gateway/example';
 import { buildUserRepository, UserRepository } from './repository/user';
 import { AdapterParams } from './types';
+import {buildFeedbackRepository, FeedbackRepository} from '@/adapter/repository/feedback';
 
 export type Adapter = {
+  feedbackRepository: FeedbackRepository;
   userRepository: UserRepository;
   exampleGateway: ExampleGateway;
 }
 
 export const buildAdapter = (params: AdapterParams): Adapter => {
   const userRepository = buildUserRepository(params);
+  const feedbackRepository = buildFeedbackRepository(params);
   const exampleGateway = buildExampleGateway(params);
 
   return {
+    feedbackRepository,
     userRepository,
     exampleGateway
   }
